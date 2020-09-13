a487ebc14fb8756dadf6da4eeaca37229820df54
Michael <g4m3rm1k3@hotmail.com>
2020-09-13 13:14:51 +0000

    variables

--- main.c
+++ main.c
@@ -1,9 +1,27 @@
+// #include <stdio.h>
+
+// int main(int argc, char const *argv[])
+// {
+//   /* code */
+//   printf("Hello World!\n");
+//   printf("My name is McLean\n");
+//   return 0;
+// }
+
+// ***********variables**************
+
 #include <stdio.h>
 
 int main(int argc, char const *argv[])
 {
-  /* code */
-  printf("Hello World!\n");
-  printf("My name is McLean\n");
+  int value = 23;
+  char character = 'a';
+  double point = 9.89;
+  float point2 = 8.90;
+
+  printf("%d\n", value);
+  printf("%c\n", character);
+  printf("%.2f\n", point);
+  printf("%.2f\n", point2);
   return 0;
 }
